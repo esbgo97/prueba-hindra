3fd6bb8f23f66063b150e37220d999a88a8e3e1d
Edward Bustos <esbgo97@gmail.com>
2020-03-13 16:58:07 -0500

    Updates

---
+++ precache-manifest.5efc31695e2ab3ba977cdfb986fb5d2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e812f618f511adae85d12a831c359b1",
+    "url": "/prueba-hindra/index.html"
+  },
+  {
+    "revision": "251adf0f856a45711489",
+    "url": "/prueba-hindra/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2516b2a8772b2c5e75de",
+    "url": "/prueba-hindra/static/js/2.bdabcaa0.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/prueba-hindra/static/js/2.bdabcaa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "251adf0f856a45711489",
+    "url": "/prueba-hindra/static/js/main.292abca3.chunk.js"
+  },
+  {
+    "revision": "b157694a0f5aa4647a2f",
+    "url": "/prueba-hindra/static/js/runtime-main.eeda3c33.js"
+  }
+]);
